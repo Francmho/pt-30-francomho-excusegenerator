5fa7c56d3644ecf3c48faaa39772abdac741b2b0
Francmho <73557750+Francmho@users.noreply.github.com>
2024-04-06 00:38:02 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  generateExcuse();
+};
+
+const generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let result = who[Math.floor(Math.random() * who.length)] + " ";
+  result += action[Math.floor(Math.random() * action.length)] + " ";
+  result += what[Math.floor(Math.random() * what.length)] + " ";
+  result += when[Math.floor(Math.random() * when.length)] + " ";
+
+  document.getElementById("excuse").innerHTML = result;
 };
